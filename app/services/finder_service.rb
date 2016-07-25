772a4959e48b21e80815ecc3a1efbac6a980cfb6
Christopher Soden <karavol27@gmail.com>
2016-07-25 10:48:37 -0600

    Commit

--- app/services/finder_service.rb
+++ app/services/finder_service.rb
@@ -1,12 +1,12 @@
 class FinderService
 
   def get_electric_and_propane(zip)
-    byebug
-    conn = Faraday.new(:url => "/api/alt-fuel-stations/")
+    conn = Faraday.new(:url => "https://api.data.gov/nrel/alt-fuel-stations/v1.json")
     response = conn.get do |req|
-      req.uri = 'v1.json'
+      # req.url = 'v1.json'
       req.params['api_key'] = ENV["nrel_api_key"]
       req.params['fuel_type'] = "ELEC, LPG"
+      req.params['zip'] = zip
     end
     result = JSON.parse(response.body)
     byebug
